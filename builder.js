803679f5792adb5fd62177652f6ecfb95136ba3f
dadinounet <tipuati@gmail.com>
2017-02-27 16:48:44 +0100

    utilisation des imports et exports des js

--- builder.js
+++ builder.js
@@ -1,3 +1,8 @@
+var string1 = require("./app/js/main/test.js");
+var string2 = require("./app/js/main/test2.js");
+
+
+document.write(string1 + string2);
 
 var singe = require("./app/js/main/timeline.js");
 
